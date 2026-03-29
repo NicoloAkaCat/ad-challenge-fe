1a43a058a7f8db116cdf948f1432469576e985c6
Nicolò Catania <101711650+NicoloAkaCat@users.noreply.github.com>
2026-03-29 20:03:45 +0200

    first commit

---
+++ app/app.vue
@@ -0,0 +1,80 @@
+<script setup lang="ts">
+import type { NavigationMenuItem } from '@nuxt/ui'
+
+const route = useRoute()
+
+useHead({
+  meta: [{ name: 'viewport', content: 'width=device-width, initial-scale=1' }],
+  link: [{ rel: 'icon', href: '/favicon.ico' }],
+  htmlAttrs: {
+    lang: 'en'
+  }
+})
+
+const title = 'Ad Challenge - Frontend'
+const description = 'Nuxt application that satisfies challenge requirements '
+
+useSeoMeta({
+  title,
+  description,
+  ogTitle: title,
+  ogDescription: description
+})
+
+const items = computed<NavigationMenuItem[]>(() => [
+  {
+    label: 'Registration',
+    to: '/auth/register',
+    active: route.path.startsWith('/auth/register')
+  },
+  {
+    label: 'Login',
+    to: '/auth/login',
+    active: route.path.startsWith('/auth/login')
+  },
+  {
+    label: 'Users',
+    to: '/users'
+  }
+])
+</script>
+
+<template>
+  <UApp>
+    <UHeader>
+      <template #left>
+        <NuxtLink to="/">
+          <AppLogo class="w-auto h-6 shrink-0" />
+        </NuxtLink>
+      </template>
+
+      <UNavigationMenu :items="items" orientation="horizontal" />
+
+      <template #body>
+        <UNavigationMenu
+          :items="items"
+          orientation="vertical"
+          class="-mx-2.5"
+        />
+      </template>
+
+      <template #right>
+        <UColorModeButton />
+      </template>
+    </UHeader>
+
+    <UMain>
+      <NuxtPage />
+    </UMain>
+
+    <USeparator icon="i-simple-icons-nuxtdotjs" />
+
+    <UFooter>
+      <template #default>
+        <p class="text-sm text-muted">
+          Built with Nuxt UI • © {{ new Date().getFullYear() }}
+        </p>
+      </template>
+    </UFooter>
+  </UApp>
+</template>
---
+++ app/pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <UPageHero
+      title="Ad Challenge - Frontend"
+      description="A simple Nuxt application that satisfies the requirements of the challenge. It consumes the API exposed by the NestJS backend that enable a full authentication flow."
+      :links="[
+        {
+          label: 'Sign up',
+          to: '/auth/register',
+          trailingIcon: 'i-lucide-arrow-right',
+          size: 'xl'
+        },
+        {
+          label: 'Sign In',
+          to: '/auth/login',
+          icon: 'i-lucide-user',
+          size: 'xl',
+          color: 'neutral',
+          variant: 'subtle'
+        }
+      ]"
+    />
+  </div>
+</template>
